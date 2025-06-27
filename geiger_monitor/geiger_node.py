b8a7427dc66b60d22536c31728c3a773413f0ff4
SasaKuruppuarachchige <sasa.kuruppuarachchi@gmail.com>
2025-06-28 02:50:07 +0300

    stamped message

--- geiger_monitor/geiger_node.py
+++ geiger_monitor/geiger_node.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32
+from marti_common_msgs.msg import Float32Stamped
 import smbus
 import struct
 import time
@@ -8,7 +8,7 @@ import time
 class GeigerI2CNode(Node):
     def __init__(self):
         super().__init__('geiger_i2c_node')
-        self.publisher_ = self.create_publisher(Float32, 'radiation_level', 10)
+        self.publisher_ = self.create_publisher(Float32Stamped, 'radiation_level', 10)
         self.bus = smbus.SMBus(1)  # Use correct I2C bus for Orin NX
         self.address = 0x08
         self.timer = self.create_timer(1.0, self.read_and_publish)
@@ -17,8 +17,10 @@ class GeigerI2CNode(Node):
         try:
             data = self.bus.read_i2c_block_data(self.address, 0, 4)
             value = struct.unpack('f', bytes(data))[0]
-            msg = Float32()
-            msg.data = value
+            msg = Float32Stamped()
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.header.frame_id = "geiger_sensor"
+            msg.value = value
             self.publisher_.publish(msg)
             self.get_logger().info(f'I2C Radiation: {value:.3f} uSv/h')
         except Exception as e:
